6541e69394b427d0f74e904332eb5e0406aaf23c
boonenf <fboonen@elsevier.com>
2019-05-02 16:59:27 +0200

    added Eva's costuming blog

--- _thisisdrachenwald/fetch_rss.py
+++ _thisisdrachenwald/fetch_rss.py
@@ -7,7 +7,7 @@ import requests
 
 rssUrls=[]
 
-with open('_data/thisisdrachenwald_feedlist.json', 'r') as f:
+with open('../_data/thisisdrachenwald_feedlist.json', 'r') as f:
     rssUrls = json.load(f)
 
 
@@ -34,9 +34,10 @@ for rssUrl in rssUrls:
 
 srtd = sorted(results, key=lambda i: i['published'], reverse=True)
 
-with io.open('_data/thisisdrachenwald.json', 'w', encoding='utf-8') as outfile:
+with io.open('../_data/thisisdrachenwald.json', 'w', encoding='utf-8') as outfile:
     json.dump(srtd[0:50], outfile, ensure_ascii=False)
 
 #for i in srtd[0:20]:
 #    print("%s: %s - %s" %(i['published'],i['site'],i['title']))
+
 
