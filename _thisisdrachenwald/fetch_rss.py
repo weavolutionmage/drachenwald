6ac0a10dcbe7908c08d78e36ce03584634227a41
boonenf <fboonen@elsevier.com>
2019-06-03 20:50:18 +0200

    adding features to handle images

--- _thisisdrachenwald/fetch_rss.py
+++ _thisisdrachenwald/fetch_rss.py
@@ -2,22 +2,13 @@ import feedparser
 import json
 from bs4 import BeautifulSoup
 import io
-import time
 import requests
 import shutil
 import os, sys
 from PIL import Image
-
-
-rssUrls=[]
-
-with open('_data/thisisdrachenwald_feedlist.json', 'r') as f:
-    rssUrls = json.load(f)
-import requests
-import shutil
-import os, sys
-from PIL import Image
-
+import hashlib
+#import pprint
+#pp = pprint.PrettyPrinter(indent=4)
 
 rssUrls=[]
 
@@ -26,14 +17,17 @@ with open('_data/thisisdrachenwald_feedlist.json', 'r') as f:
 
 
 
+rssUrls=[]
+
+with open('_data/thisisdrachenwald_feedlist.json', 'r') as f:
+    rssUrls = json.load(f)
 
 
-
+#rssUrls=[{"url": "https://huysuylenburgh.wordpress.com/feed/", "name": "Huys Uylenburgh", "link":"https://huysuylenburgh.wordpress.com","merge": False,"showMedia":  True}, {"name":  "Lia's Flickr feed", "url": "https://api.flickr.com/services/feeds/photos_public.gne?id=90046361@N00&lang=en-us&format=rss_200&tag=Drachenwald", "link":  "https://www.flickr.com/photos/liabucket/", "merge": true, "showMedia":  true}]
 
 
 results = {}
 
-print(rssUrls)
 thumbDir = "renderedImages"
 dlDir = "_dlImages"
 if (not os.path.isdir(thumbDir)):
@@ -55,11 +49,18 @@ for rssUrl in rssUrls:
             mergePosts = rssUrl["merge"]
 
         feedResults = {}
-
         for entry in NewsFeed.entries:
             title = entry['title']
             summaryTxt = entry['summary']
-            soup  = BeautifulSoup(summaryTxt, features="html.parser")
+            mediaTxt = summaryTxt
+            #Wordpress doesn't have the images in the summary field, based on generator using a different field
+            try:
+                if (NewsFeed.channel["generator"] == 'http://wordpress.com/'):
+                    mediaTxt = entry["content"][0]["value"]
+            except Exception as e:
+                mediaTxt = summaryTxt
+                
+            soup  = BeautifulSoup(mediaTxt, features="html.parser")
             summary = soup.get_text()
             images = []
             for img in soup.findAll('img'):
@@ -75,31 +76,42 @@ for rssUrl in rssUrls:
             #retrieve image and convert to thumbnail
             if showMedia and (len(images) > 0):
                 imageUrl = images[0]
+                filehash = hashlib.md5(imageUrl.encode()).hexdigest()
+
                 fnName = "%s" % imageUrl[imageUrl.rfind('/')+1:len(imageUrl)]
+                fnName = filehash
                 fn = "%s/%s" % (dlDir, fnName)
                 if (not os.path.isfile(fn)):
-                    response = requests.get(images[0], stream=True)
+                    user_agent = {'User-agent': 'This is Drachenwald'}
+                    response = requests.get(imageUrl, stream=True, headers=user_agent)
                     with open(fn, 'wb') as out_file:
                         shutil.copyfileobj(response.raw, out_file)
 
 
                 size = 250, 250
 
-                file, ext = os.path.splitext(fnName)
+                #file, ext = os.path.splitext(fnName)
+                file = fnName
                 thumbFn = "%s/%s.thumbnail.jpg" % (thumbDir,file)
+                width=0
+                height=0
                 if (not os.path.isfile(thumbFn)):
                     im = Image.open(fn)
                     im.thumbnail(size)
                     im.save(thumbFn, "JPEG")
+                    width, height = im.size
+                else:
+                    im = Image.open(fn)
+                    width, height = im.size
 
-                imgDict = {"imgSrc": imageUrl, "thumb":thumbFn}
+                imgDict = {"imgSrc": imageUrl, "thumb":thumbFn, "width":width, "height": height}
                 imageLst.append(imgDict)
 
             #    del response
 
             key = "%s%s%s" % (published.tm_year, published.tm_yday, rssUrl['name'])
 
-            postDict =  {'summary': summaryLtd, 'link': link, 'published': published, 'title': title, 'site': rssUrl['name'],
+            postDict =  {'summary': summaryLtd, 'link': link, 'published': published, 'title': title,
                  'images': imageLst}
 
             if mergePosts:
@@ -108,16 +120,13 @@ for rssUrl in rssUrls:
                     lst.append(postDict)
                     results[key]["lst"]=lst
                 else:
-                    results[key]={"lst":[postDict], "merge": True}
+                    results[key]={"lst":[postDict], "merge": True, 'site': rssUrl['name'], "siteLink": rssUrl["link"]}
             else:
-                results[key]={"lst":[(postDict)],"merge":False}
+                results[key]={"lst":[(postDict)],"merge":False, 'site': rssUrl['name'], "siteLink": rssUrl["link"]}
 
 
     except Exception as e:
-        print("%s" % e)
-
-#import pprint
-#pp = pprint.PrettyPrinter(indent=4)
+        print("error: %s" % e)
 
 
 
@@ -126,10 +135,10 @@ srtd =  [results[key] for key in sorted(results.keys(), reverse=True)]
 
 
 with io.open('_data/thisisdrachenwald.json', 'w', encoding='utf-8') as outfile:
-    json.dump(srtd[0:50], outfile, ensure_ascii=False)
+    json.dump(srtd[0:100], outfile, ensure_ascii=False)
 
 
-pp.pprint(srtd[0:50])
+#pp.pprint(srtd[0:100])
 
 #for i in srtd[0:20]:
 #    print("%s: %s - %s -%s" %(i['published'],i['site'],i['title'], i['images']))
