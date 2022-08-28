65ab043d47d031351488abeb2419f052be4bd6a6
Anna Wilson <git@annawilson.eu>
2022-08-28 12:15:18 +0200

    try deploy

---
+++ _bin/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+eval $(ssh-agent -s)
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
+echo "${OVH_PRIVATE}" | tr -d '\r' | ssh-add - > /dev/null
+JEKYLL_ENV=production bundle exec jekyll build -d public
+rsync -rvzc --delete -e 'ssh -p 22' public/ $WEBHOST_OVH:drach-main/public/
