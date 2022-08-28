fef211ba97b5caab3cfe7325903dc0bc1594bb2a
Anna Wilson <git@annawilson.eu>
2022-08-28 14:41:18 +0200

    put htaccess in the right spot

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -7,4 +7,5 @@ echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 echo "${OVH_PRIVATE}" | tr -d '\r' | ssh-add - > /dev/null
 JEKYLL_ENV=production bundle exec jekyll build -d public
-rsync -rvzc --delete -e 'ssh -p 45333' public/ $WEBHOST_OVH:drach-main/public/
+rsync -rvzc --exclude '.htaccess' --delete -e 'ssh -p 45333' public/ $WEBHOST_OVH:drach-main/public/
+rsync -rvzc --delete -e 'ssh -p 45333' .htaccess $WEBHOST_OVH:drach-main/
