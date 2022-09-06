6d8f686bc453066bf06fc4b63960b05c01b4c1eb
Finne Boonen <finne.boonen@gmail.com>
2022-09-06 21:09:22 +0200

    updated format to allow for metadatat

--- _server_scripts/google_sheets_to_json.py
+++ _server_scripts/google_sheets_to_json.py
@@ -34,26 +34,67 @@ if __name__ == '__main__':
     # Call the Sheets API
     for p in pullLst["sheets"]:
         try:
+				
             print(p)
             SAMPLE_SPREADSHEET_ID = p["sheetId"]
-            SAMPLE_RANGE_NAME = p["sheet"]
+
             dest = p["dest"]
 
             print (f"working on {dest}")
             sheet = service.spreadsheets()
-            result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+
+            if "sheet" in p.keys():
+              SAMPLE_RANGE_NAME = p["sheet"]            
+              result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                      range=SAMPLE_RANGE_NAME).execute()
+              values = result.get('values', [])
+              if values:
+
+                  max_col = len(values[0])
+                  #all rows need to be the same length, Google Sheets API doesn't return cells without values
+                  corrected_values = [x + [''] * (max_col - len(x))for x in values]
+                  #liquid works better with lower case letters and no spaces in the fieldnames
+                  header_row = [x.lower().replace(' ','-').replace('/','-') for x in corrected_values[0]]
+                  df = pd.DataFrame(corrected_values[1:], columns=header_row).fillna('')
+                  df.to_json(dest, orient="records")
+            elif "data" in p.keys() and "meta" in p.keys():
+              print(p)
+              SAMPLE_RANGE_NAME = p["data"]
+
+              print (f"working on {dest}")
+              sheet = service.spreadsheets()
+              result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+              values = result.get('values', [])
+              data = None
+              if values:
+                  max_col = len(values[0])
+                  #all rows need to be the same length, Google Sheets API doesn't return cells without values
+                  corrected_values = [x + [''] * (max_col - len(x))for x in values]
+                  #liquid works better with lower case letters and no spaces in the fieldnames
+                  header_row = [x.lower().replace(' ','-').replace('/','-') for x in corrected_values[0]]
+                  df = pd.DataFrame(corrected_values[1:], columns=header_row).fillna('')
+                  data={"data":df.to_json(orient="records")}
+
+              SAMPLE_RANGE_NAME = p["meta"]
+
+              print (f"working on {SAMPLE_RANGE_NAME}")
+              sheet = service.spreadsheets()
+              result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                     range=SAMPLE_RANGE_NAME).execute()
 
-            values = result.get('values', [])
-            if values:
-                max_col = len(values[0])
-                #all rows need to be the same length, Google Sheets API doesn't return cells without values
-                corrected_values = [x + [''] * (max_col - len(x))for x in values]
-                #liquid works better with lower case letters and no spaces in the fieldnames
-                header_row = [x.lower().replace(' ','-').replace('/','-') for x in corrected_values[0]]
-                df = pd.DataFrame(corrected_values[1:], columns=header_row).fillna('')
-                df.to_json(dest, orient="records")
-
+              values = result.get('values', [])
+              metadata={} 
+              if values:
+                  max_col = len(values[0])
+                  #all rows need to be the same length, Google Sheets API doesn't return cells without values
+                  corrected_values = [x + [''] * (max_col - len(x))for x in values]
+                  for key,value in corrected_values:
+                      metadata[key]=value	
+                  data["metadata"]=metadata
+                  
+              with open(dest, 'w', encoding='utf-8') as f:
+                  json.dump(data, f, ensure_ascii=False, indent=4)    
         except HttpError as err:
             print(err)
 
